462d75a3d8ec7606ba774dfdd1331e507c44ec30
angel gabriel hernandez rodrigez <angel_n_n@live.com>
2017-11-14 22:24:46 -0600

    Agregar archivos de proyecto.

---
+++ ConsoleApp9/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Promedio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, suma = 0, iterador = 0;
+            do
+            {
+                Console.WriteLine("Escribe un numero: ");
+                num = int.Parse(Console.ReadLine());
+                iterador++;
+                suma += num;
+            } while (num != 0);
+            Console.WriteLine("Tu promedio es: " + suma / (iterador - 1));
+            Console.ReadKey();
+        }
+    }
+}
